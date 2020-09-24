c3e28e26cb347631e37ec1fd59eb6d09f2cda0dc
juanf9224 <juanfh.24.6@gmail.com>
2020-09-24 19:59:40 -0400

    - add more test to the api - add coverage

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -6,17 +6,23 @@ import { posts } from './fixtures/posts';
 
 const mock = new MockAdapter(axios);
 
-mock.onGet('http://jsonplaceholder.typicode.com/posts').reply(200, posts);
 
-describe('Posts test', () => {
-  it('gets posts from typicode and send them to the client', (done) => {
-    server
+describe('Index test', () => {
+  it('should get posts from typicode', async () => {
+    mock.onGet('http://jsonplaceholder.typicode.com/posts').reply(200, posts);
+    const res = await server
       .get(`${BASE_URL}/posts`)
-      .expect(200)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body.data[0]).to.deep.equal(posts[0]);
-        done();
-      });
+      .expect(200);    
+    expect(res.status).to.equal(200);
+    expect(res.body.data[0]).to.deep.equal(posts[0]);
+  });
+
+  it('should return server error', async () => {
+    mock
+      .onGet('http://jsonplaceholder.typicode.com/posts')
+      .reply(500, 'Internal server error');
+    await server
+      .get(`${BASE_URL}/posts`)
+      .expect(500);
   });
 });
--- server/test/setup.js
+++ server/test/setup.js
@@ -1,7 +1,7 @@
 import supertest from 'supertest';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
-import app from '../src/app';
+import app from '../app';
 
 chai.use(sinonChai);
 export const { expect } = chai;
