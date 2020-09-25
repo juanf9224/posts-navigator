d62376aa79f887b84d42ce9bf511fbd079a80721
juanf9224 <juanfh.24.6@gmail.com>
2020-09-25 15:31:11 -0400

    - add handler to serve index.html

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
+import path from 'path';
 
 const app = express();
 
@@ -10,5 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
+
+console.log('path', path.join(__dirname));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname), '../build/index.html');
+});
 
 export default app;
